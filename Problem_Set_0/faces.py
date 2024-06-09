bbc59dc1eba3a2e61ed8eedd72e4d0e7fd0fb050
Esteban Cárdenas Rodríguez <142185391+EvanNeuroCode@users.noreply.github.com>
2024-06-09 12:22:22 -0500

    Create faces.py
    In a file called faces.py, implement a function called convert that accepts a str as input and returns that same input with any :) converted to 🙂 (otherwise known as a slightly smiling face) and any :( converted to 🙁 (otherwise known as a slightly frowning face). All other text should be returned unchanged.
    
    Then, in that same file, implement a function called main that prompts the user for input, calls convert on that input, and prints the result. You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input. Be sure to call main at the bottom of your file.

---
+++ Problem_Set_0/faces.py
@@ -0,0 +1,7 @@
+def translation(variable):
+            variable = variable.replace( ":)","🙂")
+            variable = variable.replace(":(","🙁")
+            print(variable)
+
+variable=input("Type you message ")
+translation(variable)
